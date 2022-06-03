ab77473275f1fcbc62ae5fe51dc696d7918c575b
gabycode <86427446+gabycode@users.noreply.github.com>
2022-06-03 16:08:14 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById("btn")
+btn.addEventListener("click", () => {
+  fetch("https://apis.scrimba.com/bored/api/activity")
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById("activity").textContent = data.activity
+  })
+})
